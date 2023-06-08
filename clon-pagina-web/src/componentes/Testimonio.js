152dd1cd51a5929815541e57db919ae339358567
Eduardo Wingrove <Binario01101@gmail.com>
2023-06-08 16:28:44 -0600

    Update 2

---
+++ clon-pagina-web/src/componentes/Testimonio.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Testimonio() {
+    return(
+        <div className="contenedor-testimonio">
+            <img 
+            className='imagen-testimonio'
+            src={require('../imagenes/testimonio-emma.png')}
+            alt='Foto de emma'
+            />
+
+            <div className='contenedor-texto-testimonio'>
+                <p className='nombre-testimonio'>Emma Bostia en suecia</p>
+                <p className='cargo-testimonio'>Ingeniera de software en spotify</p>
+                <p className='texto-testimonio'>Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify</p>
+            </div>
+
+        </div>
+    );
+}
+
+export default Testimonio;
