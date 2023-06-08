c7fd4ac350058c8f7dfe183d3f91bf1175f3ae6b
Eduardo Wingrove <Binario01101@gmail.com>
2023-06-08 16:59:02 -0600

    update 3

--- clon-pagina-web/src/App.js
+++ clon-pagina-web/src/App.js
@@ -4,7 +4,10 @@ import Testimonio from './componentes/Testimonio';
 function App() {
   return (
     <div className="App">
+      <div className='contenedor-principal'> 
+      <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
       <Testimonio/>
+      </div>
     </div>
   );
 }
